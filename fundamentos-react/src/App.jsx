5b26636efeb6bbdb9147a6765151bd113710524e
renan tiengo <renan.l.tiengo@gmail.com>
2020-10-06 18:51:22 -0300

    exercicios 4,5,6

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,11 +4,25 @@ import Primeiro from './components/Primeiro.jsx'
 import ComParametro from './components/ComParametro.jsx'
 import ComFilhos from './components/ComFilhos.jsx'
 import Card from './components/layout/Card.jsx'
-
+import Repeticao from './components/Repeticao.jsx'
+import Condicional from './components/Condicional.jsx'
+import CondicionalComIf from './components/CondicionalComIf'
 
 
 export default props => 
   <div className ="App">
+    <Card titulo = " #06 - Condicional v2">
+      <CondicionalComIf numero={10}></CondicionalComIf>
+    </Card>
+
+    <Card titulo = " #05 - Condicional v1">
+      <Condicional numero={11}></Condicional>
+    </Card>
+    
+    <Card titulo = " #04 - Repetição">
+      <Repeticao></Repeticao>
+    </Card>
+    
     <Card titulo = " #03 - Componente com Filhos">
       <ComFilhos>
         <ul>
@@ -27,8 +41,7 @@ export default props =>
 
     <Card titulo = " #01 - Primeiro Componente">
        <Primeiro />
-    </Card>
-    
+    </Card>    
 
 
   </div>
---
+++ fundamentos-react/src/components/Repeticao.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import produtos from '../data/produtos.js'
+
+export default props => {
+
+    function getProdutosListItem() {
+        return produtos.map(prod => {
+            return <li key={prod.id}>
+                {prod.id} - {prod.nome} -&gt; R$ {prod.preco}
+            </li>
+        })
+      
+    }
+
+    return(
+        <div>
+            <h2>Repetição</h2>
+            <ul>
+                {getProdutosListItem()}
+            </ul>
+        </div>
+    )
+       
+}
